f65a7c73d8ce2eb9d92099aee6461f88bb6075ec
maqing01 <475986855@qq.com>
2018-09-20 21:40:35 +0800

    close up earth-component-scripts

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -156,9 +156,6 @@ module.exports = {
                             // This is a feature of `babel-loader` for webpack (not Babel itself).
                             // It enables caching results in ./node_modules/.cache/babel-loader/
                             // directory for faster rebuilds.
-                            babelrc: false,
-                            presets: [require.resolve('babel-preset-react-app')],
-                            compact: true,
                             cacheDirectory: true,
                         },
                     },
